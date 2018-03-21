ab7b27dc195d5918a13f9872fbb5747d54e912fc
Brent Carabott <brent.carabott@gmail.com>
2018-03-21 18:17:18 -0400

    NHL API initial tests and setup

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://statsapi.web.nhl.com/api/v1/teams/`, {
+    }).then((res) => {
+      const teams = res.data.teams;
+      teams.map((team) => {
+      const teamID = team.id;
+        axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${teamID}/roster`, {
+        }).then((newRes) => {
+          const roster = newRes.data.roster;
+          roster.map((player) => {
+            const playerID = player.person.id;
+            axios.get(`https://statsapi.web.nhl.com/api/v1/people/${playerID}/?expand=person.stats&stats=gameLog`, {
+            }).then((playerList) => {
+              const playerObject = playerList.data.people[0];
+              // console.log(playerObject);
+            })
+          })
+        })
+
+      })
+    }) 
+  }
     render() {
       return (
         <div>
-          Hello
+          
         </div>
       )
     }
