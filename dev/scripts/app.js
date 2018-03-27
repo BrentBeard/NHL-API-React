90cb5350e7fc0daae27b5af76e5d9579b3748980
Brent Carabott <brent.carabott@gmail.com>
2018-03-27 13:23:48 -0400

    Pulling more stats

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      player : "",
+      playerObject: {} 
+    };
+    this.search = this.search.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    // this.secondStats = this.secondStats.bind(this);
+  }
 
-  componentDidMount() {
+  handleChange(e) {
+    this.setState({
+      [e.target.id] : e.target.value
+    });
+  }
+
+  search(e) {
+    e.preventDefault();
     axios.get(`https://statsapi.web.nhl.com/api/v1/teams/`, {
     }).then((res) => {
       const teams = res.data.teams;
@@ -18,7 +35,28 @@ class App extends React.Component {
             axios.get(`https://statsapi.web.nhl.com/api/v1/people/${playerID}/?expand=person.stats&stats=gameLog`, {
             }).then((playerList) => {
               const playerObject = playerList.data.people[0];
-              // console.log(playerObject);
+              if (playerObject.fullName === this.state.player) {
+                this.setState({
+                  playerObject
+                }) 
+
+                console.log(playerObject.stats[0].splits[0].season)
+                console.log(playerObject.stats[0].splits[0].stat);
+                let secondID = this.state.playerObject.id
+                console.log(playerObject)
+                axios.get(`https://statsapi.web.nhl.com/api/v1/people/${secondID}/?expand=person.stats&stats=yearByYear`, {
+                }).then((res) => {
+                  let secondStats = res.data.people[0];
+                  // console.log(secondStats);
+                  let seasons = secondStats.stats[0].splits;
+                  seasons.map((season) => {
+                    if(season.league.name === `National Hockey League`) {
+                      console.log(season.stat.shotPct);
+                    }
+                  })
+                }) 
+              } 
+
             })
           })
         })
@@ -26,9 +64,27 @@ class App extends React.Component {
       })
     }) 
   }
+
+  // secondStats() {
+  //   if(this.state.playerObject !== '') {
+  //     let secondID = this.state.playerObject.id
+  //     axios.get(`https://statsapi.web.nhl.com/api/v1/people/${secondID}/?expand=person.stats&stats=yearByYear`, {
+  //     }).then((res) => {
+  //       console.log(res);
+  //     }) 
+  //   } 
+  // } 
+
+
     render() {
       return (
         <div>
+          <form onSubmit={this.search}>
+          <label htmlFor="player">Player Name:</label>
+            <input type="text" value={this.state.player} id="player" onChange={this.handleChange} />
+            <input type="submit"/>
+            <p>{this.state.playerObject.fullName}</p>
+          </form>
           
         </div>
       )
