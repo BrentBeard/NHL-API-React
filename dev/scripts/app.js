76dadb4096e5c3f75c7c7ad084f78765e16e7a9c
Brent Carabott <brent.carabott@gmail.com>
2018-03-29 12:07:41 -0400

    added per60 stats

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -32,28 +32,93 @@ class App extends React.Component {
           const roster = newRes.data.roster;
           roster.map((player) => {
             const playerID = player.person.id;
-            axios.get(`https://statsapi.web.nhl.com/api/v1/people/${playerID}/?expand=person.stats&stats=gameLog`, {
+            axios.get(`https://statsapi.web.nhl.com/api/v1/people/${playerID}/?expand=person.stats&stats=statsSingleSeason`, {
             }).then((playerList) => {
               const playerObject = playerList.data.people[0];
               if (playerObject.fullName === this.state.player) {
                 this.setState({
                   playerObject
-                }) 
+                })
 
-                console.log(playerObject.stats[0].splits[0].season)
-                console.log(playerObject.stats[0].splits[0].stat);
+                // console.log(playerObject.stats[0].splits[0].season)
+                // console.log(playerObject.stats[0].splits[0].stat);
                 let secondID = this.state.playerObject.id
-                console.log(playerObject)
-                axios.get(`https://statsapi.web.nhl.com/api/v1/people/${secondID}/?expand=person.stats&stats=yearByYear`, {
+                let playerSeason = playerObject.stats[0].splits[0].stat
+                let iceTime = parseInt(playerSeason.timeOnIce)
+                // console.log(iceTime)
+              
+                let perSixty =  (iceTime/60)
+                console.log(playerSeason);
+                console.log(perSixty);
+                let shots60 = Math.round((playerSeason.shots / perSixty) *10 ) / 10;
+                console.log(`shots60  ${shots60}`);
+                let shotPct = playerSeason.shotPct;
+                console.log(`sh%  ${shotPct}`);
+                let goals60 = Math.round((playerSeason.goals / perSixty) *10 ) / 10;
+                console.log(`goals60  ${goals60}`);
+                let points60 = Math.round((playerSeason.points / perSixty) *10 ) / 10;
+                console.log(`points60  ${points60}`)
+
+
+            // axios.get(`https://statsapi.web.nhl.com/api/v1/people/${playerID}/?expand=person.stats&stats=gameLog`, {
+            // }).then((playerList) => {
+            //   const playerObject = playerList.data.people[0];
+            //   if (playerObject.fullName === this.state.player) {
+            //     this.setState({
+            //       playerObject
+            //     }) 
+
+            //     // console.log(playerObject.stats[0].splits[0].season)
+            //     // console.log(playerObject.stats[0].splits[0].stat);
+            //     let secondID = this.state.playerObject.id
+            //     let playerGameLog = playerObject.stats[0].splits
+            //     console.log(playerGameLog)
+
+
+
+            //     for(let i = playerGameLog.length; i <= 0; i = i - 1){
+            //     let roll = playerGameLog.pop()
+            //     let tenGame = []
+            //     tenGame.push(roll);
+            //     while(tenGame.length === 10){
+            //     console.log(tenGame);
+            //     }
+            //     }
+
+            //     while(playerGameLog.length) {
+            //       // console.log(playerGameLog.splice(0, 10));
+            //       let tenGameArray= playerGameLog.splice(0, 10);
+            //       console.log(tenGameArray)
+            //       tenGameArray.map((game) => {
+            //         console.log(game.stat.shotPct) 
+                  
+            //         // console.log(tenGameTotal)
+            //       })
+            //     }
+
+                axios.get(`https://statsapi.web.nhl.com/api/v1/people/${secondID}/?expand=person.stats&stats=careerRegularSeason`, {
                 }).then((res) => {
-                  let secondStats = res.data.people[0];
-                  // console.log(secondStats);
-                  let seasons = secondStats.stats[0].splits;
-                  seasons.map((season) => {
-                    if(season.league.name === `National Hockey League`) {
-                      console.log(season.stat.shotPct);
-                    }
-                  })
+                  let playerCareer = res.data.people[0].stats[0].splits[0].stat;
+                  console.log(playerCareer);
+                  let iceTimeCareer = parseInt(playerCareer.timeOnIce)
+
+                  let perSixtyCareer = (iceTimeCareer / 60)
+                  console.log(playerCareer);
+                  console.log(perSixtyCareer);
+                  let shots60Career = Math.round((playerCareer.shots / perSixtyCareer) * 10) / 10;
+                  console.log(`shots60Career  ${shots60Career}`);
+                  let shotPctCareer = playerCareer.shotPct;
+                  console.log(`sh%Career  ${shotPctCareer}`);
+                  let goals60Career = Math.round((playerCareer.goals / perSixtyCareer) * 10) / 10;
+                  console.log(`goals60Career  ${goals60Career}`);
+                  let points60Career = Math.round((playerCareer.points / perSixtyCareer) * 10) / 10;
+                  console.log(`points60Career  ${points60Career}`)
+                  // let seasons = secondStats.stats[0].splits;
+                  // seasons.map((season) => {
+                  //   if(season.league.name === `National Hockey League`) {
+                  //     console.log(season.stat);
+                  //   }
+                  // })
                 }) 
               } 
 
