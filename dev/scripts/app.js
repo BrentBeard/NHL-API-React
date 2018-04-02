372634768b7073c58bed132df6f8b9e7c2de8c08
Brent Carabott <brent.carabott@gmail.com>
2018-04-01 21:28:47 -0400

    working on autosuggest

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Autocomplete from 'react-autocomplete';
 import {Bar, Doughnut} from 'react-chartjs-2';
 
 class App extends React.Component {
@@ -102,6 +103,7 @@ class App extends React.Component {
                   chart: 'go',
                   points,
                   toi,
+                  player: '',
                   // players,
                   // [e.target.id]: e.target.value
                   // shots60,
@@ -563,11 +565,11 @@ class App extends React.Component {
       return (
         <React.Fragment>
           <h1>NHL Shooting Talent Evaluations</h1>
-          <h2>Hot or cold? Born this way? Find out, search any active NHL player.</h2>
+          <h2>Hot or cold? Born this way? Find out! Search any active NHL player.</h2>
 
           <form className="playerForm" onSubmit={this.search}>
           <label htmlFor="player">Spelling and grammer are important(for now), for most impressive results search Auston Matthews:</label>
-            <input className="playerInput" type="text" value={this.state.player} id="player" onChange={this.handleChange} />
+            <input className="playerInput" autoCapitalize="words" type="text" value={this.state.player} id="player" onChange={this.handleChange} />
             {/* <select name="" id="player">
             {this.state.players.map((player) =>{
              return <option key={player.fullName} id="" value={this.state.player}>{`${player.fullName}`}</option>
